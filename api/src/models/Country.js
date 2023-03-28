1924a3628b4d866307d3b091166cfbc9ebe6f054
Daniel Gomez <dgomez.informatica@gmail.com>
2023-03-27 23:10:37 -0300

    add model country then we inject the connection to sequelize

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,53 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    // ID (Código de tres letras), (Obligatorio)
+    id: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    // Nombre (Obligatorio)
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    // Imagen de la bandera (Obligatorio)
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    // Continente (obligatorio)
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    // Capital (Obligatorio)
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    // Subregión (NO Obligatorio)
+    subregion: {
+      type: DataTypes.STRING,
+    },
+
+    // Área (NO Obligatorio)
+    area: {
+      type: DataTypes.INTEGER,
+    },
+
+    // Población (OBLIGATORIO)
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  }, {
+    timestamps: false,
   });
 };
