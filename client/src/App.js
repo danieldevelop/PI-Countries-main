90097f5be39d741821c0f5662a55065e616c229e
Daniel Gomez <dgomez.informatica@gmail.com>
2023-03-31 00:44:41 -0300

    add route de navigation components project

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// import NavBar from "./components/NavBar/Navbar";
+import Welcome from "./components/Welcome/Welcome";
+import Home from "./components/Home/Home";
+import Error404 from "./components/Error/404/error404";
+import About from "./components/About/About";
+import AddActivities from "./components/Form/AddActivities/AddActivities";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        {/* <NavBar /> */}
+
+        <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/add-activities" component={AddActivities} />
+
+            {/* En caso de que no exista la pagina redirecciona un 404 */}
+            <Route path="*" component={Error404} />
+        </Switch>
+      </div>
+      </BrowserRouter>
   );
 }
 
